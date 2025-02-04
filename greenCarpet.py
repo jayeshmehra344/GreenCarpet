605d413ad1b12f84a8c876eb8970e0279cd5e2ca
DevanshuTripathi120 <aditheprince120@gmail.com>
2025-02-04 18:50:08 +0530

    Update Counter.txt

--- greenCarpet.py
+++ greenCarpet.py
@@ -1,7 +1,20 @@
 from git import Repo
 import os
 
-file = open("Counter.txt", "r+")
-text = file.read()
-num = int(text)
-file.write(str(num + 1))
+file_path = "Counter.txt"
+
+with open(file_path, "r+") as file:
+    text = file.read()
+    num = int(text)
+    file.seek(0)
+    file.write(str(num + 1))
+    file.truncate()
+
+repo = Repo("D:\\Python-For-Git\\")
+
+repo.git.add("--all")
+repo.index.commit("Update Counter.txt")
+
+origin = repo.remote(name="origin")
+
+origin.push()
